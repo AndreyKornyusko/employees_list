d2718767174c82ac42a74f8a6db3f1adaa20af4f
Andrey Kornyushko <kap86@ukr.net>
2020-12-09 08:15:45 +0200

    fixed form submit logic and form styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
       phone,
       hourlyRate
     });
-    this.setState({items:updatedItems})
+    // this.setState({items:updatedItems})
     this.closeAddBtnModal()
   }
 
